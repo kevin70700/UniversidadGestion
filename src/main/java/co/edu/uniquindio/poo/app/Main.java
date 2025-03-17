9f77690d81f72be8df2f5d400dcb732c782734b3
kevin70700 <kevina.riveras@uqvirtual.edu.co>
2025-03-16 23:32:21 -0500

    Update Main.java

--- src/main/java/co/edu/uniquindio/poo/app/Main.java
+++ src/main/java/co/edu/uniquindio/poo/app/Main.java
@@ -1,4 +1,5 @@
 package co.edu.uniquindio.poo.app;
+
 import co.edu.uniquindio.poo.model.*;
 
 import javax.swing.*;
@@ -8,7 +9,7 @@ public class Main {
         Universidad universidad = new Universidad();
 
         while (true) {
-            String opcionStr = JOptionPane.showInputDialog(null, "Menú:\n1. Registrar estudiante\n2. Agregar nota a estudiante\n3. Obtener estudiante con mayor promedio\n4. Actualizar nota de estudiante\n5. Mostrar notas y promedio de cada estudiante\n6. Mostrar lista de estudiantes con promedios\n7. Salir", "Gestión de Estudiantes", JOptionPane.QUESTION_MESSAGE);
+            String opcionStr = JOptionPane.showInputDialog(null, "Menú:\n1. Registrar estudiante\n2. Agregar nota a estudiante\n3. Obtener estudiante con mayor promedio\n4. Actualizar nota de estudiante\n5. Mostrar notas y promedio de cada estudiante\n6. Mostrar lista de estudiantes con promedios\n7. Mejor estudiante por nota\n8. Salir", "Gestión de Estudiantes", JOptionPane.QUESTION_MESSAGE);
             if (opcionStr == null) break;
             int opcion;
             try {
@@ -64,7 +65,24 @@ public class Main {
                     return;
                 default:
                     JOptionPane.showMessageDialog(null, "Opción inválida.");
+
+                case 8:
+                    String indiceStr = JOptionPane.showInputDialog("Ingrese el número de la nota a analizar (1 a 5):");
+                    int indiceNota;
+                    try {
+                        indiceNota = Integer.parseInt(indiceStr) - 1;
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número inválido.");
+                        break;
+                    }
+                    Estudiante mejorEstudianteNota = universidad.obtenerMejorEstudiantePorNota(indiceNota);
+                    String mensajeNota = (mejorEstudianteNota != null) ?
+                            "Mejor estudiante en esa nota: " + mejorEstudianteNota.getNombre() + " con nota " + mejorEstudianteNota.getNotas().get(indiceNota).getValor()
+                            : "No hay estudiantes o la posición de la nota es incorrecta.";
+                    JOptionPane.showMessageDialog(null, mensajeNota);
+                    break;
             }
+
         }
     }
 }
